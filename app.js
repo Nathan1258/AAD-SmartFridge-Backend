db4783ff07bcf809f566c798b0e3bc5456df124c
Nathan <nathan@ellisn.com>
2024-01-17 21:00:53 +0000

    added cors workaround

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 app.use(express.json());
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 
 const authRoute = require('./auth');
 const itemsRoute = require("./items");
