83c119b958a760402da52deeadf1cf48ee6dd028
Nathan <nathan@ellisn.com>
2024-01-23 16:26:01 +0000

    added endpoint to get all items current in stock

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Advanced Analysis and Design API
 
 ### Production endpoint
+
 **[https://aad-api.ellisn.com](https://aad-api.ellisn.com)**
+
 ### Locally hosted endpoint
+
 **[http://localhost:5021](http://localhost:5021)**
 
 ## Running the server locally
@@ -23,74 +26,81 @@ To run the server, make sure you have Node.js installed. Then, follow these step
     ```bash
    npm start
    ```
-The server will now be available at `http://localhost:5021/`. Alternatively just use the production endpoint [here](https://aad-api.ellisn.com)
+
+The server will now be available at `http://localhost:5021/`. Alternatively just use the production
+endpoint [here](https://aad-api.ellisn.com)
 
 ## Endpoints
 
 ### User Authentication
 
 - **Register a User:** `/v1/users/register`
-  - **Method:** POST
-  - **Description:** Register a new user. Default *access* is **Normal** unless specified.
-  - **Parameters:**
-    - *first_name*: **string**
-    - *last_name*: **string**
-    - (Optional) *access*: **string**
-    - *password*: **string**
-    
+    - **Method:** POST
+    - **Description:** Register a new user. Default *access* is **Normal** unless specified.
+    - **Parameters:**
+        - *first_name*: **string**
+        - *last_name*: **string**
+        - (Optional) *access*: **string**
+        - *password*: **string**
+
 
 - **Clock in a user:** `/v1/users/clock-in`
-  - **Method:** POST
-  - **Description:** Clock a user in for the day. Returns a 4 digit PIN they can use to access the web app for that day's shift.
-  - **Parameters:**
-    - *uid*: **int**
-    - *password*: **string**
+    - **Method:** POST
+    - **Description:** Clock a user in for the day. Returns a 4 digit PIN they can use to access the web app for that
+      day's shift.
+    - **Parameters:**
+        - *uid*: **int**
+        - *password*: **string**
 
 - **Change a user's access:** `/v1/users/change-user-access`
-  - **Method:** PUT
-  - **Description:** Change a user's access
-  - **Permissions:** Requires admin privileges.
-  - **Parameters:**
-    - *accessPIN*: **integer**
-    - *uid*: **string**
-    - *newAccessValue*: **integer**
+    - **Method:** PUT
+    - **Description:** Change a user's access
+    - **Permissions:** Requires admin privileges.
+    - **Parameters:**
+        - *accessPIN*: **integer**
+        - *uid*: **string**
+        - *newAccessValue*: **integer**
 
 ### Reports
 
 - **Generate a report:** `/v1/reports/generate`
-  - **Method:** GET
-  - **Description:** Generate a report.
-  - **Permissions:** Requires health and safety privileges.
+    - **Method:** GET
+    - **Description:** Generate a report.
+    - **Permissions:** Requires health and safety privileges.
 
 - **Log an action:** `/v1/reports/log-action`
-  - **Method:** POST
-  - **Description:** Logs an action to the activity log. If *uid* is not present then the current user performing the request will be used.
-  - **Parameters:**
-    - (Optional) *uid*: **int**
-    - *action*: **string**
+    - **Method:** POST
+    - **Description:** Logs an action to the activity log. If *uid* is not present then the current user performing the
+      request will be used.
+    - **Parameters:**
+        - (Optional) *uid*: **int**
+        - *action*: **string**
 
 ### Items
 
 - **Retrieve all items supported by the system:** `/v1/items`
-  - **Method:** POST
-  - **Description:** Returns an array of all items in the system.
-  - **Parameters:**
-    - *accessPIN*: **integer**
+    - **Method:** POST
+    - **Description:** Returns an array of all items in the system. If 'instock' query is present, then only items in
+      stock will be returned.
+    - **Parameters:**
+        - *accessPIN*: **integer**
+    - **Query Parameters**
+        - (Optional) *instock*: **boolean**
 
 - **Return a specific item:** `/v1/item/<item-name>`
-  - **Method:** POST
-  - **Description:** Returns a specific Item's data in the system.
-  - **Parameters:**
-    - *accessPIN*: **integer**
+    - **Method:** POST
+    - **Description:** Returns a specific Item's data in the system.
+    - **Parameters:**
+        - *accessPIN*: **integer**
 
 - **Insert an item in the Inventory:** `/v1/item/insert`
-  - **Method:** POST
-  - **Description:** Adds a specific item to the Fridge's inventory.
-  - **Parameters:**
-    - *accessPIN*: **integer**
-    - *itemID*: **integer**
-    - *quantity*: **integer**
-    - *expiryDate*: **string** (DD-MM-YY format **'24-12-24'**)
+    - **Method:** POST
+    - **Description:** Adds a specific item to the Fridge's inventory.
+    - **Parameters:**
+        - *accessPIN*: **integer**
+        - *itemID*: **integer**
+        - *quantity*: **integer**
+        - *expiryDate*: **string** (DD-MM-YY format **'24-12-24'**)
 
 ## User Verification Functions
 
--- items.js
+++ items.js
@@ -1,59 +1,121 @@
 const router = require("express").Router();
-const {verify} = require("./verify");
-const {query} = require("./sql");
-const {OKResponse, InternalServerErrorResponse, NotFoundResponse, MalformedBodyResponse} = require("./customResponses");
-const {convertToTimestamp} = require("./Utils");
+const { verify } = require("./verify");
+const { query } = require("./sql");
+const {
+  OKResponse,
+  InternalServerErrorResponse,
+  NotFoundResponse,
+  MalformedBodyResponse,
+} = require("./customResponses");
+const { convertToTimestamp } = require("./Utils");
 
-router.post("/", verify, (req,res) => {
-    return query("SELECT * FROM products").then((response) => {
-        return OKResponse(res, "Returned all items", response);
-    }).catch((error) => {
-       console.error("Error getting all products");
-       return InternalServerErrorResponse(res, "Could not fetch all products. Try again later.");
+router.post("/", verify, (req, res) => {
+  const sqlQuery = `SELECT
+                                p.productID,
+                                p.Name,
+                                i.itemID,
+                                i.quantity,
+                                i.expiryDate,
+                                i.lastUpdated
+                            FROM
+                                products p
+                            JOIN
+                                inventory i ON p.productID = i.itemID;
+`;
+
+  if (req.query.instock) {
+    return query(sqlQuery)
+      .then((response) => {
+        return OKResponse(res, "Returned all items in stock", response);
+      })
+      .catch((error) => {
+        console.error("Error getting all items in stock", error);
+        return InternalServerErrorResponse(
+          res,
+          "Could not fetch products. Try again later.",
+        );
+      });
+  }
+
+  return query("SELECT * FROM products")
+    .then((response) => {
+      return OKResponse(res, "Returned all items", response);
+    })
+    .catch((error) => {
+      console.error("Error getting all products");
+      return InternalServerErrorResponse(
+        res,
+        "Could not fetch all products. Try again later.",
+      );
     });
 });
 
-router.post("/:productName", verify, (req,res) => {
-    const productName = req.params.productName;
-    return query("SELECT * FROM products WHERE Name = ?", [productName]).then((response) => {
-        if(response <= 0) return NotFoundResponse(res, "Item does not exist.");
-        return OKResponse(res, "Item returned", response[0]);
-    }).catch((error) => {
-       console.error("Error getting all products");
-       return InternalServerErrorResponse(res, "Could not fetch all products. Try again later.");
+router.post("/:productName", verify, (req, res) => {
+  const productName = req.params.productName;
+  return query("SELECT * FROM products WHERE Name = ?", [productName])
+    .then((response) => {
+      if (response <= 0) return NotFoundResponse(res, "Item does not exist.");
+      return OKResponse(res, "Item returned", response[0]);
+    })
+    .catch((error) => {
+      console.error("Error getting all products");
+      return InternalServerErrorResponse(
+        res,
+        "Could not fetch all products. Try again later.",
+      );
     });
 });
 
-router.post("/insert", verify, async (req,res) => {
-    const { itemID, quantity, expiryDate } = req.body;
-    const expiryDateFormat = /^\d{2}-\d{2}-\d{2}$/;
+router.post("/insert", verify, async (req, res) => {
+  const { itemID, quantity, expiryDate } = req.body;
+  const expiryDateFormat = /^\d{2}-\d{2}-\d{2}$/;
 
-    if(!itemID) return MalformedBodyResponse(res, "'itemID' parameter is missing from request body");
-    if(!quantity) return MalformedBodyResponse(res, "'quantity' parameter is missing from request body");
-    if(!expiryDate) return MalformedBodyResponse(res, "'expiryDate' parameter is missing from request body");
-    if (!expiryDateFormat.test(expiryDate)) return MalformedBodyResponse(res, "'expiryDate' must be in DD-MM-YY format");
+  if (!itemID)
+    return MalformedBodyResponse(
+      res,
+      "'itemID' parameter is missing from request body",
+    );
+  if (!quantity)
+    return MalformedBodyResponse(
+      res,
+      "'quantity' parameter is missing from request body",
+    );
+  if (!expiryDate)
+    return MalformedBodyResponse(
+      res,
+      "'expiryDate' parameter is missing from request body",
+    );
+  if (!expiryDateFormat.test(expiryDate))
+    return MalformedBodyResponse(
+      res,
+      "'expiryDate' must be in DD-MM-YY format",
+    );
 
-    const sqlUpsert = `
+  const sqlUpsert = `
         INSERT INTO inventory (itemId, quantity, expiryDate)
         VALUES (?, ?, ?)
         ON DUPLICATE KEY UPDATE 
         quantity = quantity + VALUES(quantity);`;
 
-    const params = [itemID, quantity, convertToTimestamp(expiryDate)];
+  const params = [itemID, quantity, convertToTimestamp(expiryDate)];
 
-    try {
-        const result = await query(sqlUpsert, params);
-        if(result <= 0) return InternalServerErrorResponse(res, "Could not update Inventory. Please try again.");
-        return OKResponse(res, "Inventory updated successfully");
-    } catch (err) {
-        console.error(err);
-        return InternalServerErrorResponse(res, "Could not update Inventory. Please try again.");
-    }
+  try {
+    const result = await query(sqlUpsert, params);
+    if (result <= 0)
+      return InternalServerErrorResponse(
+        res,
+        "Could not update Inventory. Please try again.",
+      );
+    return OKResponse(res, "Inventory updated successfully");
+  } catch (err) {
+    console.error(err);
+    return InternalServerErrorResponse(
+      res,
+      "Could not update Inventory. Please try again.",
+    );
+  }
 });
 
-router.post("/remove", verify, (req,res) => {
-
-})
-
+router.post("/remove", verify, (req, res) => {});
 
 module.exports = router;
