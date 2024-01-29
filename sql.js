9c0331a8e22c53c86fc3c92d98373e552a1e7d93
Nathan <nathan@ellisn.com>
2024-01-29 14:49:51 +0000

    added new query builder and fixed issue where getting in stock items returned items that arent in stock

--- sql.js
+++ sql.js
@@ -1,25 +1,38 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+//
+// const pool = mysql.createPool({
+//   host: "192.168.0.71",
+//   port: process.env.SQL_PORT,
+//   user: "root",
+//   password: process.env.SQL_PASS,
+//   database: "ADA",
+// });
+//
+// const query = (sql, params) => {
+//   return new Promise((resolve, reject) => {
+//     pool.getConnection((err, conn) => {
+//       if (err) return reject(err);
+//
+//       conn.query(sql, params, (err, results) => {
+//         conn.release();
+//         if (err) return reject(err);
+//         resolve(results);
+//       });
+//     });
+//   });
+// };
+//
+// module.exports = { query };
 
-const pool = mysql.createPool({
+const knex = require("knex")({
+  client: "mysql2",
+  connection: {
     host: "192.168.0.71",
     port: process.env.SQL_PORT,
     user: "root",
     password: process.env.SQL_PASS,
     database: "ADA",
+  },
 });
 
-const query = (sql, params) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) return reject(err);
-
-            conn.query(sql, params, (err, results) => {
-                conn.release();
-                if (err) return reject(err);
-                resolve(results);
-            });
-        });
-    });
-};
-
-module.exports = {query};
+module.exports = { knex };
