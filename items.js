8e1599df21d39f36d7b5dccf1917d6343ba62356
Nathan <nathan@ellisn.com>
2024-01-30 14:28:32 +0000

    added helper function

--- items.js
+++ items.js
@@ -188,6 +188,14 @@ router.post("/expiring", verifyAdmin, async (req, res) => {
   }
 });
 
+function getWeekNumber(d) {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+  return [d.getUTCFullYear(), weekNo];
+}
+
 function generateOrderID(offsetWeeks = 0) {
   const currentDate = new Date();
   currentDate.setDate(currentDate.getDate() - offsetWeeks * 7);
